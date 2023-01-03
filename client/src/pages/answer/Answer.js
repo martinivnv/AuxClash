2f0ae814237503bfc942cb172df6f082406ad99f
martinivnv <martinivnv2002@gmail.com>
2023-01-03 14:39:17 -0500

    Add ability for users to submit a song

--- client/src/pages/answer/Answer.js
+++ client/src/pages/answer/Answer.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Searchbar from "./Searchbar";
+import axios from "axios";
 
 const Answer = () => {
+	const [song, setSong] = useState(null);
+
 	const onSearch = (e, query) => {
 		e.preventDefault();
 		console.log("form submitted ✅");
-		console.log(query);
+		searchYoutube(query);
 	};
+
+	async function searchYoutube(query) {
+		try {
+			const response = await axios.get(
+				"https://youtube.googleapis.com/youtube/v3/search",
+				{
+					params: {
+						key: process.env.REACT_APP_YOUTUBE_API_KEY,
+						part: "snippet",
+						maxResults: 1,
+						q: query,
+					},
+				}
+			);
+			setSong(response.data.items[0]);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
 	return (
 		<div>
-			<Searchbar onSearch={onSearch} />
+			{song ? (
+				`You submitted ${song.snippet.title}!`
+			) : (
+				<Searchbar onSearch={onSearch} />
+			)}
 		</div>
 	);
 };
