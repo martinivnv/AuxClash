8129a1182c81b39990887c412afae769ece46c32
martinivnv <martinivnv2002@gmail.com>
2023-01-04 14:26:46 -0500

    Establish connection from client to server socket

--- backend/server.js
+++ backend/server.js
@@ -4,40 +4,51 @@ const { Players } = require("./utils/players");
 
 // Import dependencies
 const express = require("express");
-const cors = require("cors");
 const http = require("http");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const path = require("path");
 const socketIO = require("socket.io");
 
 // Boilerplate setup
-dotenv.config({ path: ".env" });
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+console.log(process.env.PORT);
+console.log(process.env.CORS_ORIGIN);
+
 const app = express();
-
-app.use(cors());
-
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
-const io = socketIO(server);
+const io = socketIO(server, {
+	cors: {
+		origin: process.env.CORS_ORIGIN,
+		methods: ["GET", "POST"],
+	},
+});
 
 // Initialize classes
 var games = new LiveGames();
 var players = new Players();
 
-// MongoDB setup
-const uri = process.env.ATLAS_URI;
-mongoose.set("strictQuery", false);
-mongoose.connect(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-const connection = mongoose.connection;
-connection.once("open", () => {
-	console.log("Connection to MongoDB established successfully.");
-});
+// // MongoDB setup
+// const uri = process.env.ATLAS_URI;
+// mongoose.set("strictQuery", false);
+// mongoose.connect(uri, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// });
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+// 	console.log("Connection to MongoDB established successfully.");
+// });
 
 server.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
 });
 
-io.on("connection", (socket) => {});
+// When connection is made from client
+io.on("connection", (socket) => {
+	// When host connects to the server
+	socket.on("host-joins", (data) => {
+		console.log(data);
+	});
+});
--- client/src/pages/host/lobby/Lobby.js
+++ client/src/pages/host/lobby/Lobby.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import socketIOClient from "socket.io-client";
 
 const Lobby = () => {
 	const lobbyCode = "XXXXXX";
+
+	useEffect(() => {
+		console.log("Lobby useEffect");
+		const socket = socketIOClient("http://localhost:5000/");
+		console.log(socket);
+		socket.on("connect", () => {
+			console.log("Connected to the server");
+			// Lets server know this is a host connection
+			socket.emit("host-join", "Host joined!");
+		});
+
+		socket.on("disconnect", () => {
+			console.log("Disconnected from the server");
+		});
+
+		return () => {
+			socket.disconnect();
+		};
+	}, []);
 
 	return (
 		<div>
