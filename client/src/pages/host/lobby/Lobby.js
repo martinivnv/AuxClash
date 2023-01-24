93ee4fdbe88a7a2ce69bf0b52cbdbccd47e36e97
martinivnv <martinivnv2002@gmail.com>
2023-01-23 21:13:42 -0500

    Working on lobby UI

--- client/src/pages/host/lobby/Lobby.js
+++ client/src/pages/host/lobby/Lobby.js
@@ -7,7 +7,7 @@ import PlayerList from "./PlayerList";
 
 const Lobby = () => {
 	const navigate = useNavigate();
-	const [players, setPlayers] = useState(["player1", "player2"]);
+	const [players, setPlayers] = useState([]);
 	const [lobbyCode, setLobbyCode] = useState("...");
 	const currentLobbyCode = useRef("...");
 	const [category, setCategory] = useState("Party");
@@ -56,15 +56,22 @@ const Lobby = () => {
 
 	return (
 		<GameContainer>
-			<div>
-				<h3>Head to auxclash.com and enter the code below to join:</h3>
-				<p>{lobbyCode}</p>
-				<br />
-				<button onClick={emitStartGame}>Begin the Game</button>
-			</div>
-			<div>
-				<h3>Aux Warriors:</h3>
-				<PlayerList players={players} />
+			<div className="flex flex-row ">
+				<div className="w-1/2">
+					<h3>Head to auxclash.com and enter the code below to join:</h3>
+					<p className="m-8 text-4xl font-bold">{lobbyCode}</p>
+					<br />
+					<button onClick={emitStartGame} disabled={players.length < 3}>
+						Begin the Game
+					</button>
+					<p className="m-8">
+						The game can't begin until at least 3 players have joined!
+					</p>
+				</div>
+				<div className="w-1/2">
+					<h3>Aux Warriors:</h3>
+					<PlayerList players={players} />
+				</div>
 			</div>
 		</GameContainer>
 	);
--- client/src/pages/host/lobby/PlayerList.js
+++ client/src/pages/host/lobby/PlayerList.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const PlayerList = ({ players }) => {
 	return (
-		<ul>
-			{players.map((p) => (
-				<li key={p.playerId}>{p.playerName}</li>
-			))}
-		</ul>
+		<div className="m-4 flex h-full items-center justify-center rounded border">
+			{players.length > 0 ? (
+				<ul>
+					{players.map((p) => (
+						<li key={p.playerId}>{p.playerName}</li>
+					))}
+				</ul>
+			) : (
+				<p>Waiting...</p>
+			)}
+		</div>
 	);
 };
 
