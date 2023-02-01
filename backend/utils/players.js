a080a99e2cdf29f11dd8cebf692f775ec9b29415
martinivnv <martinivnv2002@gmail.com>
2023-01-31 21:59:46 -0500

    Rebuild players class to be object instead of array

--- backend/utils/players.js
+++ backend/utils/players.js
@@ -1,27 +1,27 @@
 class Players {
 	constructor() {
-		this.players = [];
+		this.players = {};
 	}
 	addPlayer(hostId, playerId, playerName, score) {
-		var player = { hostId, playerId, playerName, score };
-		this.players.push(player);
+		var player = { hostId, playerName, score };
+		this.players[playerId] = player;
 		return player;
 	}
 	removePlayer(playerId) {
 		var player = this.getPlayer(playerId);
 
-		if (player) {
-			this.players = this.players.filter(
-				(player) => player.playerId !== playerId
-			);
-		}
+		delete this.players[playerId];
+
 		return player;
 	}
 	getPlayer(playerId) {
-		return this.players.find((player) => player.playerId === playerId);
+		return { ...this.players[playerId], playerId: playerId };
 	}
-	getPlayers(hostId) {
-		return this.players.filter((player) => player.hostId === hostId);
+	updatePlayerScore(playerId, newScore) {
+		this.players[playerId].score = newScore;
+	}
+	updateHostId(playerId, newHostId) {
+		this.players[playerId].hostId = newHostId;
 	}
 }
 
