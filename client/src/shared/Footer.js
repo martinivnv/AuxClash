b2ca7cd2e80e5f78bd3f24e749b6a2758a4a423c
martinivnv <martinivnv2002@gmail.com>
2023-02-02 15:29:39 -0500

    Add links to footer

--- client/src/shared/Footer.js
+++ client/src/shared/Footer.js
@@ -1,19 +1,38 @@
 import footerLogo from "../resources/martinivnv-logo.png";
+import PrivacyPolicy from "../resources/Aux-Clash-Privacy-Policy-2023.pdf";
 
 const Footer = ({ showPrivacyPolicy }) => {
+	const onPrivacyPolicyClick = () => {
+		window.open(PrivacyPolicy);
+	};
+
 	return (
 		<div className="absolute inset-x-0 bottom-0 flex flex-col justify-between px-8 py-4 text-base sm:flex-row">
 			<div className="flex items-center justify-center">
-				<img
-					src={footerLogo}
-					alt="Logo"
-					width="35px"
-					height="35px"
-					className="mr-2 mix-blend-color-dodge"
-				/>
+				<a href="https://martinivnv.com/" target="_blank" rel="noreferrer">
+					<img
+						src={footerLogo}
+						alt="Logo"
+						width="35px"
+						height="35px"
+						className="mr-2 mix-blend-color-dodge"
+					/>
+				</a>
 				<div>© 2023 Martin Ivanov</div>
 			</div>
-			<div>{showPrivacyPolicy && "Privacy Policy"}</div>
+			<div>
+				{showPrivacyPolicy && (
+					<a
+						href
+						target="_blank"
+						rel="noreferrer"
+						onClick={onPrivacyPolicyClick}
+						className="underline"
+					>
+						Privacy Policy
+					</a>
+				)}
+			</div>
 		</div>
 	);
 };
