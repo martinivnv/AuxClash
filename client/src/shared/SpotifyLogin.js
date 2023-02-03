9d05c4a68142b7a3915e954b700dfdd5605ea05d
martinivnv <martinivnv2002@gmail.com>
2023-02-03 11:23:13 -0500

    Don't allow mobile users to host

--- client/src/shared/SpotifyLogin.js
+++ client/src/shared/SpotifyLogin.js
@@ -1,17 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const SpotifyLogin = ({ buttonText }) => {
 	const [authorizationUrl, setAuthorizationUrl] = useState(null);
+	const [width, setWidth] = useState(window.innerWidth);
+	const isMobile = width < 768;
+
+	function handleWindowSizeChange() {
+		setWidth(window.innerWidth);
+	}
+
+	useEffect(() => {
+		window.addEventListener("resize", handleWindowSizeChange);
+		return () => {
+			window.removeEventListener("resize", handleWindowSizeChange);
+		};
+	}, []);
 
 	const handleClick = async () => {
-		try {
-			const response = await fetch(
-				`${process.env.REACT_APP_SERVER_URL}/api/spotify-login`
+		if (isMobile) {
+			window.alert(
+				"You can't host a lobby on a mobile device! Please use a device with a larger screen."
 			);
-			const data = await response.json();
-			setAuthorizationUrl(data.authorizationUrl);
-		} catch (e) {
-			console.error(e);
+		} else {
+			try {
+				const response = await fetch(
+					`${process.env.REACT_APP_SERVER_URL}/api/spotify-login`
+				);
+				const data = await response.json();
+				setAuthorizationUrl(data.authorizationUrl);
+			} catch (e) {
+				console.error(e);
+			}
 		}
 	};
 
