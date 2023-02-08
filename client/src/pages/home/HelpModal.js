04be80ca9cea29957811ef1fc47695210a8580c2
martinivnv <martinivnv2002@gmail.com>
2023-02-07 23:30:29 -0500

    Add "x" button to help modal

--- client/src/pages/home/HelpModal.js
+++ client/src/pages/home/HelpModal.js
@@ -15,36 +15,44 @@ const HelpModal = ({ isOpen, onRequestClose }) => {
 			}}
 			appElement={document.getElementById("root") || undefined}
 		>
+			<button
+				onClick={onRequestClose}
+				className="absolute top-1 right-1 bg-transparent p-2 text-base text-black"
+			>
+				X
+			</button>
 			<h1 className="text-xl font-bold">What is Aux Clash?</h1>
 			<p>
 				Aux Clash is the easiest way to aux battle with your friends and create
-				the ultimate playlist! Given a prompt like{" "}
+				the ultimate playlist! <br /> <br /> Given a prompt like{" "}
 				<span className="italic">"Name a song that gives you goosebumps"</span>,
 				everyone has 90 seconds to submit a song. The songs are played, then
-				everyone has 90 seconds to vote on the best pick. At the end of 3
-				rounds, all of the songs are compiled into a playlist that can be saved
-				to the host's Spotify and one person is crowned the{" "}
+				everyone has 90 seconds to vote on the best pick. <br /> <br />
+				At the end of 3 rounds, all of the songs are compiled into a playlist
+				that can be saved to the host's Spotify and one person is crowned the{" "}
 				<span className="font-bold">Aux Champion</span>
-				{"! "}
+				{"! "} <br /> <br />
 				<span className="italic text-rose-600">
 					Note that Aux Clash works for groups of 3 to 8.
 				</span>
 			</p>
 			<h2 className="mt-6 text-xl font-bold">How does it work?</h2>
 			<p>
-				Have you ever used Kahoot? Aux Clash works just like that. <br /> The
-				host needs to sign in to their{" "}
+				Have you ever used Kahoot? Aux Clash works just like that. <br /> <br />
+				The host needs to sign in to their{" "}
 				<span className="font-bold">Spotify</span> account to start a lobby on a{" "}
-				<span className="font-bold">laptop, PC, or tablet.</span> <br />{" "}
+				<span className="font-bold">laptop, PC, or tablet.</span> <br /> <br />{" "}
 				<span className="italic">
 					Note: In order to play submitted songs to their full length, the host
 					needs to have Spotify Premium. However, the trial version will still
 					work for playing short snippets.
 				</span>{" "}
+				<br />
 				<br /> Everyone else joins on their phones using the lobby code, no
 				Spotify account required. Once 3 to 8 friends have joined, the Aux Clash
 				can begin.
 			</p>
+			<br />
 			<p>
 				Questions?{" "}
 				<a
